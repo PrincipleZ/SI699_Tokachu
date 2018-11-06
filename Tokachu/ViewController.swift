dc27b98bacbde3907f397617cb937f46f0382754
Yuanze Zhang <yuanzez@umich.edu>
2018-11-06 13:44:17 -0500

    Initial Commit

---
+++ Tokachu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tokachu
+//
+//  Created by Yuanze Zhang on 11/6/18.
+//  Copyright © 2018 Tokachu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
