a57dfc203b8460caa26529d01d0712760b86a95d
Shaung Cheng <shaungc@umich.edu>
2018-11-10 19:07:02 -0500

    create tab views

---
+++ README.md
@@ -0,0 +1,7 @@
+#  Tokachu Frontend
+
+## Walk Through
+
+- [Quick setup tab & navigation view hybrid](https://www.youtube.com/watch?v=uAtZ-xGwmmE)
+- [Basic Setup each tab view controller](https://www.youtube.com/watch?v=erUWj0gOb7k)
+
--- Tokachu/LoginViewController.swift
+++ Tokachu/LoginViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func loginTapped(_ sender: Any) {
+    }
 }
 
