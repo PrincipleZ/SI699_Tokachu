f93f1ee4a2ce716c164da606032df78b041ed460
Shaung Cheng <shaungc@umich.edu>
2018-11-10 19:26:48 -0500

    create view controllers for each tab

---
+++ Tokachu/ExploreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreViewController.swift
+//  Tokachu
+//
+//  Created by Shaung Cheng on 11/10/18.
+//  Copyright © 2018 Tokachu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExploreViewController: UIViewController {
+    
+    var username: String?
+    var password: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
