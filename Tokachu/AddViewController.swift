2b1918229d075dd5d57f2603a8966e10602e631c
Shaung Cheng <shaungc@umich.edu>
2018-11-12 18:01:43 -0500

    set keyboard dismiss for add tab view

--- Tokachu/AddViewController.swift
+++ Tokachu/AddViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class AddViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        scrollView.keyboardDismissMode = .onDrag
+        
     }
     
 
