b127104ab95eb8a149e14cec6b7de28504d5af5b
Shaung Cheng <shaungc@umich.edu>
2018-12-06 18:14:52 -0500

    for sake of demo

--- Tokachu/Explore/ExploreDatasource.swift
+++ Tokachu/Explore/ExploreDatasource.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ExploreDatasource: NSObject, UICollectionViewDataSource {
     
-    var objects: [Explore] = []
+    var objects: [Explore] = [
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "3")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "4")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "5")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "6")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "7")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "8")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "9")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "10")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "11")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "12")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "13")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "14")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "15")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "16")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "17")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "3")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "4")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "5")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "6")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "7")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "8")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "9")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "10")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "11")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "12")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "13")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "14")!),
+        Explore(title: "Arborretum Wandering", location: "Law School", image: UIImage(named: "event-image-placeholder")!),
+        Explore(title: "Drive in Erie", location: "Huran River", image: UIImage(named: "15")!),
+        Explore(title: "Lorem ipsum dolor sit", location: "Nunc tempor dui at", image: UIImage(named: "16")!),
+        Explore(title: "Vestibulum blandit augue quis", location: "Cras sodales velit nec", image: UIImage(named: "17")!),
+        Explore(title: "Pellentesque rhoncus", location: "Law School", image: UIImage(named: "6")!),
+    ]
     
     // Mark: mock data
     func fill() {
-        objects = []
+//        objects = []
 //        for i in 1...10 {
 //            objects.append(
 //                Explore(title: "Mock Data Happy Hour \(i)", location: "Law School", image: UIImage(named: "event-image-placeholder")!)
